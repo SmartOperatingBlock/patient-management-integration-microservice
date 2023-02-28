5db5caa6d353c717ff70efad7dec7ebe1e27ded7
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-02-28 14:46:58 +0100

    test(entity): add test for patient entity

---
+++ src/test/kotlin/TestEntity.kt
@@ -0,0 +1,44 @@
+import entity.Patient
+import entity.PatientData
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldNotBe
+
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+class TestEntity : StringSpec({
+
+    val patient = Patient(
+        PatientData.TaxCode("1234567890"),
+        "John",
+        "Doe",
+        "1990-05-10",
+        PatientData.Height(180.5, PatientData.LengthUnit.CENTIMETER),
+        PatientData.Weight(75.2, PatientData.MassUnit.KILOGRAM),
+        PatientData.BloodGroup.A_POSITIVE
+    )
+
+    "A patient should correctly be created" {
+        patient shouldNotBe null
+    }
+
+    "A patient with empty taxCode should throw exception" {
+        shouldThrow<IllegalStateException> {
+            Patient(
+                PatientData.TaxCode(""),
+                "John",
+                "Doe",
+                "1990-05-10",
+                PatientData.Height(180.5, PatientData.LengthUnit.CENTIMETER),
+                PatientData.Weight(75.2, PatientData.MassUnit.KILOGRAM),
+                PatientData.BloodGroup.A_POSITIVE
+            )
+        }
+    }
+})
